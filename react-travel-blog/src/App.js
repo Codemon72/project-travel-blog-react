98ee05c87012b9361ef53d6dce3d648e8551b92f
cpoellmann <christoph.poellmann@deptagency.com>
2020-06-12 16:58:38 +0200

    setup header and nav + some styling

--- react-travel-blog/src/App.js
+++ react-travel-blog/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./assets/main.css";
-import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
 import Main from "./components/Main/Main.js";
+import Header from "./components/Header/Header";
 
 function App() {
 	return (
 		<div className="App">
+			<Menu />
 			<Header />
 			<Main />
 		</div>
--- react-travel-blog/src/components/Main/Bloglist/Bloglist.js
+++ react-travel-blog/src/components/Main/Bloglist/Bloglist.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Bloglist = () => {
 	return (
-		<div className="lg:w-1/2 bg-teal-800 text-white text-center font-semibold text-3xl md:text-4xl lg:text-5xl">
-			<h1>Hier kommen alle Blogs rein</h1>
+		<div className="lg:w-1/2 bg-teal-600 text-white text-center font-semibold text-3xl md:text-4xl lg:text-5xl">
+			<h2>Hier kommen alle Blogs rein</h2>
 		</div>
 	);
-}
+};
 
 export default Bloglist;
