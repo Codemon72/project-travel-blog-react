ae8a2e5c9cdde98b572dc26d7f59b1bb8b1fcbcf
poelle <christoph.poellmann@deptagency.com>
2020-06-19 22:36:50 +0200

    implemented image loading from storage

--- react-travel-blog/src/components/Main/Preview.js
+++ react-travel-blog/src/components/Main/Preview.js
@@ -1,24 +1,29 @@
-import React from "react";
-import lake from "../../assets/img/15Peyto_Lake_Mountains_Canada800.jpg";
+import React, { useState } from "react";
 
-const Preview = ({place, selected, key}) => {
-  console.log(key)
+const Preview = ({ place, selected, key, getUrl }) => {
+	const [imageUrl, setImageUrl] = useState(null);
 
-  return (
-    <div className={"bloglist__card text-teal-900 " + ((selected && (place.title === selected.title)) ? " border-solid border-4 border-red-700 hoverEffect" : console.log(selected))} >      
-      <img
-        className="block"
-        src={lake}
-        alt="{place.title}"
-      />
-      <div className="bloglist__content">
-      <h4 className="bloglist__title">{place.title}</h4>
-      <div className="bloglist__date">visiting date: xxx</div>
-      <div className="bloglist__author"> {place.author}</div>
-      </div>
-    </div>
-  )
-}
+	getUrl(place.image.src).then((url) => {
+		setImageUrl(url);
+	});
 
-export default Preview
+	return (
+		<div
+			className={
+				"bloglist__card text-teal-900 " +
+				(selected &&
+					place.title === selected.title &&
+					" border-solid border-4 border-red-700 hoverEffect")
+			}
+		>
+			<img className="block" src={imageUrl} alt="{place.title}" />
+			<div className="bloglist__content">
+				<h4 className="bloglist__title">{place.title}</h4>
+				<div className="bloglist__date">visiting date: xxx</div>
+				<div className="bloglist__author"> {place.author}</div>
+			</div>
+		</div>
+	);
+};
 
+export default Preview;
