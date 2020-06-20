40880f5e7d64167f20261310b47b5df509eb23c1
poelle <christoph.poellmann@deptagency.com>
2020-06-20 16:55:14 +0200

    detailpage stuff

--- react-travel-blog/src/components/Details.js
+++ react-travel-blog/src/components/Details.js
@@ -5,44 +5,46 @@ import authorPic from "../assets/img/ich.png";
 
 const Details = () => {
 	const [imageUrl, setImageUrl] = useState(null);
-	const { getUrlObject, selected, places } = useContext(AppContext);
-	// const [detailEntry, setDetailEntry] = useState(null);
+	const { getUrlObject, places } = useContext(AppContext);
+	const [detailEntry, setDetailEntry] = useState("");
 	const { id } = useParams();
 
-	// const detailEntry = JSON.parse(localStorage.getItem("Place"));
+	useEffect(() => {
+		const entry = places.find((item) => item.id === id);
+		setDetailEntry(entry);
+	}, [id]);
 
-	// if (detailEntry) {
-	// 	getUrlObject(detailEntry.image.src).then((url) => {
-	// 		setImageUrl(url);
-	// 	});
-	// }
-
-	// useEffect(() => {
-	// 	const entry = places.find((item) => item.id === id);
-	// 	setDetailEntry(entry);
-	// }, [places]);
+	if (detailEntry) {
+		getUrlObject(detailEntry.image.src).then((url) => {
+			setImageUrl(url);
+		});
+	}
 
 	return (
-		<div class="relative container max-w-sm rounded overflow-hidden justify-center bg-white">
-			{/* <img class="w-full" src={imageUrl} />
-			<div class="px-6 py-4">
-				<div class="font-bold text-xl mb-2">{detailEntry.title}</div>
-				<p class=" block text-gray-700 text-base">{detailEntry.text}</p>
-			</div>
-			<div class="px-6 py-4 flex items-center">
-				<img
-					class="w-10 h-10 rounded-full mr-4"
-					src={authorPic}
-					alt="Profil picture"
-				/>
-				<div class="text-sm">
-					<p class="text-gray-900 leading-none">{detailEntry.author}</p>
-					<p class="text-gray-600">
-						{detailEntry.last_visited} in {detailEntry.location.city},{" "}
-						{detailEntry.location.country}
-					</p>
+		<div>
+			{detailEntry && (
+				<div className="relative container max-w-sm rounded overflow-hidden justify-center bg-white">
+					<img className="w-full" src={imageUrl} />
+					<div className="px-6 py-4">
+						<div className="font-bold text-xl mb-2">{detailEntry.title}</div>
+						<p className=" block text-gray-700 text-base">{detailEntry.text}</p>
+					</div>
+					<div className="px-6 py-4 flex items-center">
+						<img
+							className="w-10 h-10 rounded-full mr-4"
+							src={authorPic}
+							alt="Profil picture"
+						/>
+						<div className="text-sm">
+							<p className="text-gray-900 leading-none">{detailEntry.author}</p>
+							<p className="text-gray-600">
+								{detailEntry.last_visited} in {detailEntry.location.city},{" "}
+								{detailEntry.location.country}
+							</p>
+						</div>
+					</div>
 				</div>
-			</div> */}
+			)}
 		</div>
 	);
 };
