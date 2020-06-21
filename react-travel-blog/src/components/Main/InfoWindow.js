619724b1ef9676cd5aff7dbbef6c6a9a0480368d
Clemens Bruesch <clemensbruesch@gmail.com>
2020-06-21 13:45:24 +0200

    link in infowindows

--- react-travel-blog/src/components/Main/InfoWindow.js
+++ react-travel-blog/src/components/Main/InfoWindow.js
@@ -7,7 +7,7 @@ const InfoWindow = ({ place, closeInfo }) => {
 	return (
 		<div className="infoWindow flex flex-col cursor-default">
 			<div className="flex justify-between">
-				<h3 className="text-lg underline inline-block">{place.title}</h3>
+				<h3 className="text-xl inline-block">{place.title}</h3>
 				<div
 					className="inline-block self-start cursor-pointer closing-x"
 					onClick={closeInfo}
@@ -17,6 +17,12 @@ const InfoWindow = ({ place, closeInfo }) => {
 				last visited: {beautifyDate(place.last_visited)}
 			</p>
 			<p className="pt-3">{place.author}</p>
+			<Link to={`/details/${place.id}`} >
+			<button class="flex items-center mt-3 self-start bg-teal-500 hover:bg-teal-700 text-white font-bold py-1 px-2 border-none rounded">
+				View Details
+				<i className="fas fa-arrow-right ml-2"></i>
+			</button>
+			</Link>
 		</div>
 	);
 };
